cd93ad0218fe01847b488041f329bc8c5ef0536e
hw-lyu <lyuhye03@gmail.com>
2024-02-06 10:44:47 +0900

    feat : 기본적인 마크업 관련 레이아웃 세팅

---
+++ resources/views/common/header.blade.php
@@ -0,0 +1,20 @@
+<header class="flex flex-row justify-between px-10 bg-emerald-200">
+    <div class="group">
+        <div class="group">
+            <h1 class="common-title">Lumii</h1>
+            <div>Admin</div>
+        </div>
+        <div class="group">
+            <div>lumii</div>
+            <div>ip</div>
+        </div>
+    </div>
+    <nav>
+        <h2 class="is-blind">메뉴</h2>
+        <ul class="list">
+            <li class="item"><a href="">기본 설정</a></li>
+            <li class="item"><a href="">메뉴 관리</a></li>
+            <li class="item"><a href="">블로그 통계</a></li>
+        </ul>
+    </nav>
+</header>
