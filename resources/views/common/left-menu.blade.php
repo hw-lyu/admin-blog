02d5c933fbac4fc4767f0575e62c2b76b36d0ec6
hw-lyu <lyuhye03@gmail.com>
2024-02-11 12:20:29 +0900

    feat : 어드민 페이지 - 기본 레이아웃 구성 추가

--- resources/views/common/left-menu.blade.php
+++ resources/views/common/left-menu.blade.php
@@ -1,8 +1,12 @@
 <div class="left-wrap basis-[160px] py-5 border-r border-y-gray-200 min-h-[600px]">
     <nav>
         <h2 class="sr-only">메뉴</h2>
-        <ul class="text-[0.825em]">
-            <li>왼쪽메뉴</li>
-        </ul>
+        <div class="flex flex-col">
+            <h3 class="font-bold mb-1">기본정보관리</h3>
+            <ul class="text-[0.825em]">
+                <li><a href="">블로그 정보</a></li>
+                <li><a href="">기본 서체 정보</a></li>
+            </ul>
+        </div>
     </nav>
 </div>
