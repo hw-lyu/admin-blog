e243dd33acb4ea5f63e1cb468913315aaa6bff88
hw-lyu <lyuhye03@gmail.com>
2024-02-11 12:20:29 +0900

    feat : 어드민 페이지 - 기본 레이아웃 구성 추가

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,33 @@
 @extends('layouts.default')
 @section('right-content')
-    <div>오른쪽 콘텐츠</div>
+    <table class="table-fixed text-left">
+        <tbody>
+        <tr>
+            <th>블로그명</th>
+            <td><input type="text" class="form-input" name="name"></td>
+        </tr>
+        <tr>
+            <th>별명</th>
+            <td><input type="text" class="form-input" name="nick_name"></td>
+        </tr>
+        <tr>
+            <th>자기소개</th>
+            <td>
+                <textarea class="form-textarea" name="introduce"></textarea>
+            </td>
+        </tr>
+        <tr>
+            <th>프로필 사진</th>
+            <td>
+                <input type="file" name="profile">
+            </td>
+        </tr>
+        <tr>
+            <th>불로그 커버 이미지</th>
+            <td>
+                <input type="file" name="cover_img">
+            </td>
+        </tr>
+        </tbody>
+    </table>
 @endsection
