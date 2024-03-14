b15f488e3a178e3f14fa11e9e1b8f30641d0f5eb
hw-lyu <lyuhye03@gmail.com>
2024-03-14 20:27:19 +0900

    feat : 전역으로 블로그 메뉴, 인포메이션 데이터 공유하기 위해 뷰 파사드로 리팩토링

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+
+use App\Models\BlogMenu;
+use App\Models\BlogInformation;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,58 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 메타 데이터
+        $defaultData = $this->defaultData();
+
+        View::share($defaultData);
+    }
+
+    /**
+     * default.blade 메타 데이터
+     *
+     * @return array
+     */
+    public function defaultData(): array
+    {
+        /**
+         * 블로그 인포메이션
+         */
+        $blogInfo = BlogInformation::latest()->first() ?? [
+            'nick_name' => '',
+            'name' => '',
+            'introduce' => '',
+            'name_eng' => ''
+        ];
+
+        /*
+         * 블로그 메뉴
+         * */
+        $blogMenu = BlogMenu::selectRaw('id, name, name_eng, is_blind, sort')
+            ->where('is_blind', 1)
+            ->orderBy('sort', 'asc')
+            ->get()
+            ->toArray() ?? [
+            'id' => '',
+            'name' => '',
+            'is_blind' => '',
+            'sort' => ''
+        ];
+
+        /*
+         * 메타태그 기본값
+         * */
+        $title = trim(mb_substr($blogInfo['name'], 0, 25, 'utf-8')) . '...';
+        $introduce = trim(mb_substr($blogInfo['introduce'], 0, 25, 'utf-8')) . '...';
+        $url = Request()->url();
+
+        return [
+            'blogInfo' => $blogInfo,
+            'blogMenu' => $blogMenu,
+            'meta' => [
+                'title' => $title,
+                'introduce' => $introduce,
+                'url' => $url
+            ]
+        ];
     }
 }
--- resources/views/front/layouts/default.blade.php
+++ resources/views/front/layouts/default.blade.php
@@ -1,44 +1,11 @@
-@php
-    /*
-     * 블로그 인포메이션
-     * */
-    $blogInfo = \App\Models\BlogInformation::latest()->first() ?? [
-        'nick_name' => '',
-        'name' => '',
-        'introduce' => '',
-        'name_eng' => ''
-    ];
-
-    /*
-     * 블로그 메뉴
-     * */
-    $blogMenu = \App\Models\BlogMenu::selectRaw('id, name, name_eng, is_blind, sort')
-            ->where('is_blind', 1)
-            ->orderBy('sort', 'asc')
-            ->get()
-            ->toArray() ?? [
-            'id' => '',
-            'name' => '',
-            'is_blind' => '',
-            'sort' => ''
-        ];
-
-    /*
-     * 메타태그 기본값
-     * */
-    $title = trim(mb_substr($blogInfo['name'], 0, 25, 'utf-8')). '...';
-    $introduce = trim(mb_substr($blogInfo['introduce'], 0, 25, 'utf-8')). '...';
-    $url = Request()->url();
-@endphp
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="og:title" content="@yield('og:title', $title)"/>
-    <meta property="og:description" content="@yield('og:description', $introduce)"/>
-    <meta property="og:url" content="@yield('og:url', $url)"/>
+    <meta property="og:title" content="@yield('og:title', $meta['title'])"/>
+    <meta property="og:description" content="@yield('og:description', $meta['introduce'])"/>
+    <meta property="og:url" content="@yield('og:url', $meta['url'])"/>
     <title>블로그 - @yield('title', '메인')</title>
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     @vite(['resources/css/front-app.css', 'resources/js/front-app.js'])
