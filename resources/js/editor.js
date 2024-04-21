ffa4bcd778d72cd14473ae1e836420ea393e2fa7
hw-lyu <lyuhye03@gmail.com>
2024-04-22 02:18:23 +0900

    feat : auth 추가

--- resources/js/editor.js
+++ resources/js/editor.js
@@ -18,7 +18,10 @@ if (document?.querySelector('#editor')) {
                 axios({
                     url: '/api/v1/file/upload', method: 'post', headers: {
                         "Content-Type": "multipart/form-data",
-                    }, data: formData
+                    }, data: formData, auth: {
+                        username: USER.email,
+                        password: USER.password
+                    }
                 }).then(function (res) {
                     let data = res.data, imageSrc = document.getElementById('toastuiAltTextInput').value ?? 'image',
                         imageUrl = `![${imageSrc}](//lumii-photo.s3.ap-northeast-2.amazonaws.com/${data.post.post_path}?id=${data.post.id})`;
