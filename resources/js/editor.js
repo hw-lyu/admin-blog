cb0568036d100d31f31f34efa4e54a26f6f5f60f
hw-lyu <lyuhye03@gmail.com>
2024-04-22 12:15:26 +0900

    feat : 어드민 페이지 plugin color syntax 추가

--- resources/js/editor.js
+++ resources/js/editor.js
@@ -2,6 +2,10 @@ import Editor from '@toast-ui/editor';
 import '@toast-ui/editor/dist/i18n/ko-kr';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
+import 'tui-color-picker/dist/tui-color-picker.css';
+import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
+
 if (document?.querySelector('#editor')) {
     let forms = document.forms, content = document.querySelector('textarea[name="content"]');
     const editor = new Editor({
@@ -10,6 +14,7 @@ if (document?.querySelector('#editor')) {
         previewStyle: 'vertical',
         height: '500px',
         initialValue: '',
+        plugins: [colorSyntax],
         hooks: {
             addImageBlobHook: function (e) {
                 let formData = new FormData();
