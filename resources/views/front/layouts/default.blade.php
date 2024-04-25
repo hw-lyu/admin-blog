bd0929293f246c8d83747358ef6e5b1db8037d9e
hw-lyu <lyuhye03@gmail.com>
2024-04-25 23:28:24 +0900

    feat : 프론트 페이지 - 폰트 SUIT로 변경

--- resources/views/front/layouts/default.blade.php
+++ resources/views/front/layouts/default.blade.php
@@ -7,9 +7,6 @@
     <meta property="og:description" content="@yield('og:description', $meta['introduce'])"/>
     <meta property="og:url" content="@yield('og:url', $meta['url'])"/>
     <title>블로그 - @yield('title', '메인')</title>
-    <link rel="preload" href="//cdn.df.nexon.com/img/common/font/DNFForgedBlade-Light.otf" as="font" type="font/otf" crossorigin="anonymous">
-    <link rel="preload" href="//cdn.df.nexon.com/img/common/font/DNFForgedBlade-Medium.otf" as="font" type="font/otf" crossorigin="anonymous">
-    <link rel="preload" href="//cdn.df.nexon.com/img/common/font/DNFForgedBlade-Bold.otf" as="font" type="font/otf" crossorigin="anonymous">
     @vite(['resources/css/front-app.css', 'resources/js/front-app.js'])
 </head>
 <body>
